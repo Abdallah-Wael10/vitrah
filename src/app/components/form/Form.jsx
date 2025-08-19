9f3cdd302913fa0a55be3761a2332ff0a62e8fbb
Abdallah Wael <bedowael365@gmail.com>
2025-08-19 18:21:25 +0300

    update form

--- src/app/components/form/Form.jsx
+++ src/app/components/form/Form.jsx
@@ -1,28 +1,35 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Form = () => {
+  const formRef = useRef(null);
+
   useEffect(() => {
     const script = document.createElement("script");
     script.async = true;
-    script.setAttribute("data-b24-form", "inline/9/fkvwvi");
+    script.setAttribute("data-b24-form", "inline/11/igp0nk");
     script.setAttribute("data-skip-moving", "true");
-    script.setAttribute("data-b24-form-container", "bitrix-form-root");
     script.src =
-      "https://cdn.bitrix24.com/b31758109/crm/form/loader_9.js?" +
+      "https://cdn.bitrix24.com/b31758109/crm/form/loader_11.js?" +
       ((Date.now() / 180000) | 0);
-    document.body.appendChild(script);
+
+    if (formRef.current) {
+      formRef.current.appendChild(script);
+    }
 
     return () => {
-      document.body.removeChild(script);
+      if (formRef.current && script.parentNode === formRef.current) {
+        formRef.current.removeChild(script);
+      }
     };
   }, []);
 
   return (
-    <section className="w-full h-max pt-5 pb-5 flex justify-center items-center ">
-      <div className="w-full h-max flex justify-center items-center">
-        {/* هنا هيظهر الفورم من Bitrix */}
-        <div id="bitrix-form-root" className="w-full"></div>
+    <section className="w-full h-max bg-[#1C1C1C]  pt-5">
+      <div className="w-full h-max pb-5 flex justify-center items-center">
+        
+        {/* سيتم حقن الفورم هنا */}
+        <div ref={formRef} className="w-full "></div>
       </div>
     </section>
   );
